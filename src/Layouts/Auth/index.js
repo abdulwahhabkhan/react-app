16aa4deaf59b2280fb7a9735a4faba292f0c18b5
Abdul Wahhab Khan <abdul.webequator.com>
2020-03-30 23:10:14 +0100

    index page converted to react-bootstrap

--- src/Layouts/Auth/index.js
+++ src/Layouts/Auth/index.js
@@ -1,27 +1,48 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
-import logo from "../../images/logos/logo.svg";;
+import logo from "../../images/logos/logo.svg";
+import {Container, Navbar,Nav, NavItem, NavLink} from "react-bootstrap";
 
 const Header = (props) =>{
     return(
-        <div className="header navbar navbar-expand-md navbar-transparent fixed-top">
-            <div className="container">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" to="/">
+        <div>
+            <Navbar variant={'transparent'} expand={'md'} fixed={'top'} className={'header'}>
+                <Container>
+                    <Navbar.Brand href="#home">
                         <img src={logo} alt="brand-logo" className="brand-image"/>
-                    </Link>
-                </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav ml-md-auto">
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-link">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/register" className="nav-link">Register</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse>
+                        <Nav className={'nav ml-md-auto'}>
+                            <NavItem>
+                                <NavLink as={Link} to="/login">Login</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink as={Link} to="/register">Register</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            {/*<div className="header navbar navbar-expand-md navbar-transparent fixed-top">
+                <Container>
+                    <div className="navbar-header">
+                        <Link className="navbar-brand" to="/">
+                            <img src={logo} alt="brand-logo" className="brand-image"/>
+                        </Link>
+                    </div>
+                    <div className="collapse navbar-collapse">
+                        <ul className="nav navbar-nav ml-md-auto">
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-link">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/register" className="nav-link">Register</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </Container>
+            </div>*/}
         </div>
     )
 };
