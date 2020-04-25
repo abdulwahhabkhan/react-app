d31c2fbaa539e128bb7245b90f20d9162fe1a540
Abdul Wahhab Khan <abdul.webequator.com>
2020-04-25 13:26:26 +0100

    connect application to redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import './bootstrap.js';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from "react-router-dom";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
+
 const app = (
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+    </Provider>
 );
-ReactDOM.render(app, document.getElementById('root'));
+
+
+ReactDOM.render( app, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/actions.js
@@ -0,0 +1,17 @@
+//project actions
+
+export const PROJECT = {
+    ADD: 'ADD',
+    EDIT: 'EDIT',
+    DELETE: 'DELETE',
+    VIEW: 'VIEW',
+}
+
+//ticket actions
+
+export const TICKET = {
+    ADD: 'ADD',
+    EDIT: 'EDIT',
+    DELETE: 'DELETE',
+    VIEW: 'VIEW',
+}
