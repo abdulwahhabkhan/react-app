7323168f5ea4d6df7595e29d2cbb40f6491b6ffc
Abdul Wahhab Khan <abdul.webequator.com>
2020-05-02 12:10:24 +0100

    auth check added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './style/app.scss';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import user from './services/auth'
 
 //import Person from './Pages/Person/Person';
 import routes from "./routes";
@@ -12,19 +13,29 @@ window.settings = settings;
 
 class App extends Component {
 
-  render() {
-    return (
-        <Router>
-          <Switch>
-              {routes.map((route, idx) =>
-                  <Route path={route.path} component={route.component} key={idx} exact={!route.children} />
-              )}
-              <Route component={NotFound}></Route>
-          </Switch>
-        </Router>
 
-    );
-  }
+    render() {
+        const PrivateRoute = ({component: Component, ...rest}) => (
+            <Route {...rest} render={(props) => (
+                user.isUserAuthenticated() === true
+                    ? <Component {...props} />
+                    : <Redirect to='/logout'/>
+            )}/>
+        )
+        return (
+            <Router>
+                <Switch>
+                    {routes.map((route, idx) =>
+                        route.isPublic ?
+                            <Route path={route.path} component={route.component} key={idx} exact={!route.children}/> :
+                            <PrivateRoute path={route.path} component={route.component} key={idx} exact={!route.children} />
+                    )}
+                    <Route component={NotFound}></Route>
+                </Switch>
+            </Router>
+
+        );
+    }
 }
 
 export default App;
