09e03c6de725fbd3073de848b7d1846659412a1c
Abdul Wahhab Khan <abdul.webequator.com>
2020-07-07 00:09:01 +0100

    blank slate component created

---
+++ src/Components/UI/Slate/BlankSlate.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const BlankSlate = (props) => {
+    return (
+        <div className="blank-slate">
+            <div className="slate-body">
+                <div className="slate-image">
+                    <img src={props.image} />
+                </div>
+                <div className="slate-text">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlankSlate
