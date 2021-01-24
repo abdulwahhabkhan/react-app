feae83f4af954118711d510e59680d544f8069ca
Abdul Wahhab Khan <>
2021-01-24 23:28:23 +0000

    error response added

--- src/Pages/Projects/Form/index.js
+++ src/Pages/Projects/Form/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Button, Modal, Form, Tabs, Tab, Row, Col} from "react-bootstrap";
+import {Button, Modal, Form, Tabs, Tab, Row, Col, Alert} from "react-bootstrap";
 import CalenderInput from '../../../Components/UI/Form';
 import Select  from "react-select";
 import settings from "../../../Config/settings";
@@ -22,6 +22,8 @@ class ProjectForm extends Component {
         progress: 0,
         loading: false,
         activeTab : this.props.tab ? this.props.tab: 'description',
+        errorMessage : '',
+        errors:[],
         ...this.props.project
     }
 
@@ -51,13 +53,14 @@ class ProjectForm extends Component {
             .then(response => {
                 this.setState({'loading': false});
                 console.log(response);
-                //todo: show success message
                 this.props.onClose(false);
                 this.props.onUpdate(response);
             })
             .catch(response => {
+                const errors = response.response.data;
                 this.setState({'loading': false});
-                //todo: show errors
+                this.setState({'errorMessage': errors.message});
+                this.setState({'errors': errors.errors});
             })
     }
 
@@ -82,7 +85,9 @@ class ProjectForm extends Component {
 
 
     render() {
-        const title = this.props.project ? 'Edit Project' : 'Add Project';
+        const title = this.props.project ? 'Edit Project' : 'Add Project'
+        const errorMessage = this.state.errorMessage
+        const errors = this.state.errors
         return (
             <Modal show={this.props.show} size={'lg'} centered onHide={()=> this.props.onClose(false)}>
                 <Modal.Header closeButton>
@@ -90,12 +95,28 @@ class ProjectForm extends Component {
                 </Modal.Header>
                 <Modal.Body>
                     <form action="" className="form-horizontal">
+                        {
+                            errorMessage !== "" && (
+                                <Alert variant={'danger'}>
+                                    <Alert.Heading as={'h6'}>{errorMessage}</Alert.Heading>
+                                    {
+                                        /*errors && (
+                                            <ul>
+                                                {errors.map((error, index)=><li key={index}>{error}</li>)}
+                                            </ul>
+
+                                        )*/
+                                    }
+                                </Alert>
+                            )
+                        }
                         <Form.Group>
                             <Form.Label>Project title is:</Form.Label>
                             <Form.Control value={this.state.name}
                                           name={'name'}
                                           placeholder={'like react application'}
                                           onChange={this.handleChange} />
+                                          <Form.Control.Feedback type={'invalid'}>Required</Form.Control.Feedback>
                         </Form.Group>
                         <div className="nav-htabs">
                             <Tabs defaultActiveKey={this.state.activeTab} id="project_form_tabs">
